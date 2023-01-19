776d70a622ecbbd1f86889f62b5ef24122cf69f2
JuliaJSJ <juliajs@yandex.ru>
2023-01-18 19:52:38 -0500

    Изменения

--- src/main/java/ru/netology/springboot_2/repository/UserRepository.java
+++ src/main/java/ru/netology/springboot_2/repository/UserRepository.java
@@ -3,21 +3,36 @@ package ru.netology.springboot_2.repository;
 import org.springframework.stereotype.Repository;
 import ru.netology.springboot_2.model.Authorities;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 @Repository
 public class UserRepository {
+    Map<String, String>  users = new HashMap<>();
+    Map<String, ArrayList<Authorities>> acls = new HashMap<>();
+
+    UserRepository() {
+        this.addUser("admin", "1111", new Authorities[]{Authorities.DELETE, Authorities.WRITE, Authorities.READ});
+        this.addUser("user", "2222", new Authorities[]{Authorities.WRITE, Authorities.READ});
+    }
+
     public List<Authorities> getUserAuthorities(String user, String password) {
-        List<Authorities> authorities = new ArrayList<>();
-        if (user.equals("root") && password.equals("1234")) {
-            Collections.addAll(authorities, Authorities.READ, Authorities.WRITE, Authorities.DELETE);
-        } else if (user.equals("admin") && password.equals("4321")) {
-            Collections.addAll(authorities, Authorities.READ, Authorities.WRITE);
-        } else if (user.equals("user") && password.equals("3333")) {
-            Collections.addAll(authorities, Authorities.READ);
+
+        if (users.get(user).equals(password)) {
+            if (acls.containsKey(user)) {
+                return acls.get(user);
+            }
         }
-        return authorities;
+
+        return new ArrayList<>();
+    }
+
+    private void addUser(String user, String password, Authorities[] acl) {
+        users.put(user, password);
+
+        ArrayList<Authorities> tempAcls = new ArrayList<Authorities>();
+        for (int i = acl.length - 1; i >= 0; i--) {
+            tempAcls.add(acl[i]);
+        }
+        acls.put(user, tempAcls);
     }
 }
